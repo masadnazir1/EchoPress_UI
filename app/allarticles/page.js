e4f7bc2e00bfd29aa53dbd548dbf8bd7e3010045
masadnazir1 <masadnazir1@gmail.com>
2025-05-01 16:16:51 +0500

    added the image placeholder image and all articles page --asad

--- app/allarticles/page.js
+++ app/allarticles/page.js
@@ -5,21 +5,18 @@ import ArticleCard from "../components/ArticleCard";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-export default function allarticles() {
-  const Router = useRouter();
+export default function AllArticles() {
+  const router = useRouter();
+
   return (
     <div className={styles.page}>
       <div className={styles.HeaderArea}>
-        <button className={styles.backBTN} onClick={() => Router.back()}>
+        <button className={styles.backBTN} onClick={() => router.back()}>
           <Image src={BackImg} width={30} height={30} alt="Back" />
         </button>
       </div>
       <main className={styles.main}>
         <div style={{ width: "100%", marginTop: "50px" }}>
-          <ArticleCard />
-          <ArticleCard />
-          <ArticleCard />
-          <ArticleCard />
           <ArticleCard />
         </div>
       </main>
